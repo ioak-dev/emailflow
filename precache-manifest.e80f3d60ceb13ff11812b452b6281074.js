30567f306cbc3f53ea379f0b88922d18e03d03cb
Arun Kumar Selvaraj <dev@ioak.org>
2021-01-20 18:19:27 +0530

    Updates

---
+++ precache-manifest.e80f3d60ceb13ff11812b452b6281074.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18da66f9d5b448583cd7c816d0b37854",
+    "url": "/index.html"
+  },
+  {
+    "revision": "35f18d7797710bb5b075",
+    "url": "/static/css/main.0b2a7d67.chunk.css"
+  },
+  {
+    "revision": "b293cfbe414735708cb3",
+    "url": "/static/js/2.7fff0366.chunk.js"
+  },
+  {
+    "revision": "c6add204db1feac3a9caaab2162d8b1e",
+    "url": "/static/js/2.7fff0366.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35f18d7797710bb5b075",
+    "url": "/static/js/main.949364fa.chunk.js"
+  },
+  {
+    "revision": "a300bef0821970c40d91",
+    "url": "/static/js/runtime-main.c5fb22d9.js"
+  },
+  {
+    "revision": "5d65b00bb12411618929f81953c003eb",
+    "url": "/static/media/emailflow_white.5d65b00b.svg"
+  },
+  {
+    "revision": "bc9580be27db18160cd8075f5679f0d3",
+    "url": "/static/media/login_bg.bc9580be.jpg"
+  }
+]);
